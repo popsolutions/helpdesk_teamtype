001acb1df1fe76d41e034b52402f0c2bec9970a9
MateusONunes <mateus.2006@gmail.com>
2022-01-17 12:22:39 -0300

    Simple adjustament

--- static/src/js/helpdesk_ticket_teamtype_js.js
+++ static/src/js/helpdesk_ticket_teamtype_js.js
@@ -21,16 +21,15 @@ odoo.define('helpdesk_ticket_teamtyoe.js', function (require) {
         typeids = typeids.replace('helpdesk.ticket.type', '');
         typeids = typeids.replace('(', '');
         typeids = typeids.replace(')', '');
-        typeids = typeids + ',';
+        typeids = ' ' + typeids + ',';
 
         var type = document.getElementById("type_id");
 
         Array.from(type.getElementsByTagName('option')).forEach(function(element){
-            element.hidden = (typeids.indexOf(element.value + ',') == -1);
+            element.hidden = (typeids.indexOf(' ' + element.value + ',') == -1);
         });
 
         type.selectedIndex = -1;
         $("#type_id").show();
     });
 });
-
